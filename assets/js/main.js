26027897a0de7cef9bbe2bf164af4f49dd29af85
manupendratiwari <manupendratiwari@gmail.com>
2023-05-28 01:34:00 +0530

    Added Portfolio details

---
+++ assets/js/main.js
@@ -0,0 +1,153 @@
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$main = $('#main');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Nav.
+		var $nav = $('#nav');
+
+		if ($nav.length > 0) {
+
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav.addClass('alt');
+						},
+						leave: function() {
+							$nav.removeClass('alt');
+						},
+					});
+
+			// Links.
+				var $nav_a = $nav.find('a');
+
+				$nav_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav.height(); }
+					})
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							$nav_a
+								.removeClass('active')
+								.removeClass('active-locked');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
+
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
+
+								},
+								enter: function() {
+
+									// Activate section.
+										$section.removeClass('inactive');
+
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($nav_a.filter('.active-locked').length == 0) {
+
+											$nav_a.removeClass('active');
+											$this.addClass('active');
+
+										}
+
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+
+								}
+							});
+
+					});
+
+		}
+
+		// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000
+		});
+
+		// Dark Mode toggler
+		$('input[type="checkbox"]').click(function(){
+			if($(this).prop("checked") == true){
+				toggleDarkMode();
+			}
+			else if($(this).prop("checked") == false){
+				toggleLightMode();
+			}
+		});
+
+})(jQuery);
+
+
+// toggle dark mode
+function toggleDarkMode() {
+	var mainbody = document.getElementById("main");
+	mainbody.classList.add("dark-mode");
+	mainbody.classList.remove("light-mode");
+
+	var showmore = document.getElementById("show-more");
+	showmore.classList.add("dark-mode");
+	showmore.classList.remove("light-mode");
+	
+ }
+
+// toggle light mode
+ function toggleLightMode() {
+	var mainbody = document.getElementById("main");
+	mainbody.classList.add("light-mode");
+	mainbody.classList.remove("dark-mode");
+
+	var showmore = document.getElementById("show-more");
+	showmore.classList.add("light-mode");
+	showmore.classList.remove("dark-mode");
+
+ }
